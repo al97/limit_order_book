967e471e8b7ad0a241b43817dacc71c5340a0516
Albert <albertjinkim@gmail.com>
2026-08-30 23:06:11 -0400

    making tests work

--- cpp/tests/types_test.cpp
+++ cpp/tests/types_test.cpp
@@ -1,3 +1,13 @@
+#include <cassert>
+
 #include "lob/types.hpp"
 
-lob::Order order = { 1, lob::Side::Buy, 100, 100 };
+int main() {
+  const lob::Order order = { 1, lob::Side::Buy, 100, 100 };
+  assert(order.id == 1);
+  assert(order.side == lob::Side::Buy);
+  assert(order.quantity == 100);
+  assert(order.price == 100);
+
+  return 0;
+}
