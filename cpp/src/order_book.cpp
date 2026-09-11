3a0dbdaadbb3d9e9bfd242815cfb6d2eb8316fd5
Albert <albertjinkim@gmail.com>
2026-09-10 23:48:45 -0400

    milestone 1

--- cpp/src/order_book.cpp
+++ cpp/src/order_book.cpp
@@ -3,17 +3,98 @@
 namespace lob {
 
 void OrderBook::AddOrder(const Order& order) {
-  orders.push_back(order);
+  if (order.side == Side::Buy) {
+    auto& queue = buySideMap[order.price];
+    queue.push_back(order);
+    locator[order.id] = std::prev(queue.end());
+  }
+  else {
+    auto& queue = sellSideMap[order.price];
+    queue.push_back(order);
+    locator[order.id] = std::prev(queue.end());
+  }
 }
+
 Quantity OrderBook::GetVolumeAtPriceAndSide(
   PriceTicks price, Side side) const {
   Quantity quantity = 0;
-  for (const Order& order : orders) {
-    if (price == order.price && side == order.side) {
+  if (side == Side::Buy) {
+    const auto it = buySideMap.find(price);
+    if (it == buySideMap.end()) {
+      return 0;
+    }
+    for (const Order& order : it->second) {
+      quantity += order.quantity;
+    }
+  } else {
+    const auto it = sellSideMap.find(price);
+    if (it == sellSideMap.end()) {
+      return 0;
+    }
+    for (const Order& order : it->second) {
       quantity += order.quantity;
     }
   }
   return quantity;
+}
+
+Quantity OrderBook::GetRestingQuantity(OrderId id) const {
+  const auto it = locator.find(id);
+  if (it == locator.end()) {
+    return 0;
+  }
+  
+  return (*it->second).quantity;
+}
+
+LevelSnapshot OrderBook::Top(Side side) {
+  LevelSnapshot level;
+  if (side == Side::Buy && !buySideMap.empty()) {
+    const auto& [price, queue] = *buySideMap.begin();
+    level.price = price;
+    for (const Order& order : queue) {
+      level.quantity += order.quantity;
+    }
+  }
+  else if (side == Side::Sell && !sellSideMap.empty()) {
+    const auto& [price, queue] = *sellSideMap.begin();
+    level.price = price;
+    for (const Order& order : queue) {
+      level.quantity += order.quantity;
+    }
+  }
+  return level;
+}
+
+std::vector<LevelSnapshot> OrderBook::Depth(Side side, std::size_t levels) const {
+  std::vector<LevelSnapshot> level_snap;
+  if (side == Side::Buy) {
+    for (const auto& [price, order_queue] : buySideMap) {
+      LevelSnapshot snap_at_price;
+      snap_at_price.price = price;
+      for (const auto& order : order_queue) {
+        snap_at_price.quantity += order.quantity;
+      }
+      level_snap.push_back(snap_at_price);
+      if (level_snap.size() == levels) {
+        break;
+      }
+    }
+  }
+  else {
+    for (const auto& [price, order_queue] : sellSideMap) {
+      LevelSnapshot snap_at_price;
+      snap_at_price.price = price;
+      for (const auto& order : order_queue) {
+        snap_at_price.quantity += order.quantity;
+      }
+      level_snap.push_back(snap_at_price);
+      if (level_snap.size() == levels) {
+        break;
+      }
+    }
+  }
+  return level_snap;
 }
 
 } //namespace lob
--- cpp/tests/order_book_test.cpp
+++ cpp/tests/order_book_test.cpp
@@ -42,12 +42,46 @@ void TestWrongPriceExcluded() {
   assert(book.GetVolumeAtPriceAndSide(50, lob::Side::Buy) == 50);
 }
 
+void TestTopIsAccurate() {
+  lob::OrderBook book;
+  const lob::Order order = {1, lob::Side::Buy, 100, 100};
+  book.AddOrder(order);
+  const lob::Order smallerOrder = {2, lob::Side::Buy, 90, 100};
+  book.AddOrder(smallerOrder);
+  lob::LevelSnapshot top = book.Top(lob::Side::Buy);
+  assert(top.price == 100);
+}
+
+void TestDepthCombinesMultipleOrdersAtSamePrice() {
+  lob::OrderBook book;
+  const lob::Order order = {1, lob::Side::Buy, 100, 100};
+  book.AddOrder(order);
+  const lob::Order smallerOrder = {2, lob::Side::Buy, 100, 100};
+  book.AddOrder(smallerOrder);
+  std::vector<lob::LevelSnapshot> levels = book.Depth(lob::Side::Buy, 2);
+  assert(levels[0].price == 100);
+  assert(levels[0].quantity == 200);
+}
+
+void TestGetRestingQuantityFindsRightOrder() {
+  lob::OrderBook book;
+  const lob::Order order = {1, lob::Side::Buy, 100, 100};
+  book.AddOrder(order);
+  const lob::Order smallerOrder = {2, lob::Side::Buy, 90, 90};
+  book.AddOrder(smallerOrder);
+  assert(book.GetRestingQuantity(1) == 100);
+  assert(book.GetRestingQuantity(2) == 90);
+}
+
 int main() {
   TestEmptyBook();
   TestOneAddedOrder();
   TestTwoAddedOrders();
   TestWrongPriceExcluded();
   TestWrongSideExcluded();
+  TestTopIsAccurate();
+  TestDepthCombinesMultipleOrdersAtSamePrice();
+  TestGetRestingQuantityFindsRightOrder();
 
   return 0;
 }
